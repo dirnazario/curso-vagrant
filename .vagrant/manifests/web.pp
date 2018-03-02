add273d3a66344f36ce51db7ee6efeeb32b65706
dirnazario <dir_jacui@hotmail.com>
2018-03-02 14:38:23 -0300

    Aula02 - Exercicio-1

--- .vagrant/manifests/web.pp
+++ .vagrant/manifests/web.pp
@@ -8,3 +8,18 @@ package { ["openjdk-7-jre", "tomcat7"]:
 exec { "apt-get-install-unzip":
   command => "/usr/bin/apt-get install unzip"
 }
+service { "tomcat7":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+    require => Package["tomcat7"]
+}
+file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
+    source => "/vagrant/.vagrant/manifests/vraptor-musicjungle.war",
+    owner => "tomcat7",
+    group => "tomcat7",
+    mode => 0644,
+    require => Package["tomcat7"],
+    notify => Service["tomcat7"]
+}
