9a1eb14ae6b70ce70c2f605647326cb853357951
dirnazario <dir_jacui@hotmail.com>
2018-03-02 18:08:58 -0300

    Aula 3 - install mysql

--- .vagrant/manifests/web.pp
+++ .vagrant/manifests/web.pp
@@ -1,7 +1,7 @@
 exec { "apt-update":
   command => "/usr/bin/apt-get update"
 }
-package { ["openjdk-7-jre", "tomcat7"]:
+package { ["openjdk-7-jre", "tomcat7", "mysql-server"]:
   ensure => installed,
   require => Exec["apt-update"]
 }
@@ -15,15 +15,14 @@ service { "tomcat7":
     hasrestart => true,
     require => Package["tomcat7"]
 }
-file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
-    source => "/vagrant/.vagrant/manifests/vraptor-musicjungle.war",
-    owner => "tomcat7",
-    group => "tomcat7",
-    mode => 0644,
-    require => Package["tomcat7"],
-    notify => Service["tomcat7"]
+service { "mysql":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+    require => Package["mysql-server"]
 }
-file { "/var/lib/tomcat7/webapps/vraptor-segunda-aplicacao.war":
+file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
     source => "/vagrant/.vagrant/manifests/vraptor-musicjungle.war",
     owner => "tomcat7",
     group => "tomcat7",
