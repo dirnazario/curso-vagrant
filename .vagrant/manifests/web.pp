0964648a982d02c3a08e037fc6647d301c510a48
dirnazario <dir_jacui@hotmail.com>
2018-03-03 12:51:14 -0300

    aula 04 - funcoes

--- .vagrant/manifests/web.pp
+++ .vagrant/manifests/web.pp
@@ -30,15 +30,19 @@ file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
     require => Package["tomcat7"],
     notify => Service["tomcat7"]
 }
-exec { "musicjungle":
-    command => "mysqladmin -uroot create musicjungle",
-    unless => "mysql -u root musicjungle",
-    path => "/usr/bin",
-    require => Service["mysql"]
+exec { "create-musicjungle-user":
+    unless => "/usr/bin/mysql -umusicjungle -pminha-senha musicjungle",
+    command => "/usr/bin/mysql -uroot -e \"GRANT ALL PRIVILEGES ON * TO musicjungle@localhost IDENTIFIED BY 'minha-senha';\" musicjungle",
+    require => Service["mysql"],
 }
-exec {"user":
-    command => "mysql -uroot -e \"GRANT ALL PRIVILEGES ON * TO 'musicjungle'@'%' IDENTIFIED BY 'minha-senha';\" musicjungle",
-    unless => "mysql -u musicjungle musicjungle",
-    path => "/usr/bin",
-    require => Service["mysql"]
+file_line { "production":
+    file => "/etc/default/tomcat7",
+    line => "JAVA_OPTS=\"\$JAVA_OPTS -Dbr.com.caelum.vraptor.environment=production\"",
+    require => Package["tomcat7"],
+    notify => Service["tomcat7"]
+}
+define file_line($file, $line) {
+    exec { "/bin/echo '${line}' >> '${file}'":
+        unless => "/bin/grep -qFx '${line}' '${file}'"
+    }
 }
